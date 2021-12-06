ec6b7bb5f3f04cdd55a64d87aadd89ae7e1def28
yla382 <yoonhlee0831@gmail.com>
2021-12-05 22:57:23 -0800

    initial page added

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import "../stylesheets/AboutMe.scss";
+
+export const AboutMe = () => {
+    return (
+        <div className="AboutMeContainer">
+            <div className="typistContainer">
+                Landing
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
---
+++ src/components/Landing.js
@@ -0,0 +1,20 @@
+import "../stylesheets/Landing.scss";
+import {BsGithub, BsLinkedin} from "react-icons/bs";
+
+
+export const Landing = () => {
+    return (
+        <div className="landingContainer">
+            <div className="typistContainer">
+                <h1>Hello, I'm John</h1>
+                <p>Software Systems Student from SFU</p>
+                <div className="iconContainer">
+                    <a href="https://github.com/yla382" target="_blank" rel="noreferrer"><BsGithub/></a>
+                    <a href="https://www.linkedin.com/in/john-lee-361641114/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
