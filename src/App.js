7e46cf1f33ab681dc095485a0f255edb2f8310db
yla382 <yoonhlee0831@gmail.com>
2022-05-09 15:40:20 -0700

    removed about me page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import Navigation from './components/Navigation';
 import Landing from './components/Landing';
-import AboutMe from './components/AboutMe';
+// import AboutMe from './components/AboutMe';
 import Timeline from './components/Timeline';
 import Skills from './components/Skills';
 import { useRef } from 'react';
