f0f183f04c739e52058602e8e9cbdddfc7b0c011
yla382 <yoonhlee0831@gmail.com>
2022-05-11 15:27:36 -0700

    carousel added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,19 @@
 import "../stylesheets/Projects.scss";
 import "../App.scss";
+import Carousel from 'react-elastic-carousel'
 
 export const Projects =({ scrollProjects }) => {
     return (
         <div className="pageContainer projects_container" ref={ scrollProjects }>
             <div>
                 <h1>Projects</h1>
+            </div>
+            <div className="sliderContainer">
+            <Carousel itemsToShow={1}>
+                <div>Project 1</div>
+                <div>Project 2</div>
+                <div>Project 3</div>
+            </Carousel>
             </div>
         </div>
     );
