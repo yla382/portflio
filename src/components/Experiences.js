2324924789eba1e1fc8dfb4a10006f3418cebe82
yla382 <yoonhlee0831@gmail.com>
2022-05-13 02:57:21 -0700

    experience added

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,11 +1,29 @@
 import "../stylesheets/Experiences.scss";
 import "../App.scss";
+import greenLightLogo from "../props/greenlight.svg";
 
 export const Experiences = ({ scrollExperiences }) => {
     return (
         <div className="pageContainer experiences_container" ref={ scrollExperiences }>
-            <div>
-                <h1>Experiences</h1>
+            <h1>Experiences</h1>
+            <div className="experience">
+                <img src={greenLightLogo} alt=''/>
+                <div className="employment">
+                    <h3>Greenlight Innovation Inc</h3>
+                    <div className="jobTitle">
+                        <p>Web Developer</p>
+                        <p>January 2021 - December 2021</p>
+                    </div>
+                    <div className="jobDescription">
+                        <p>
+                            this hish is hish ishi fffffffffffffffffffffffffffff
+                            ffffffffffffffffffffffffffff ffffffffffffffffffffffffffff
+                            ddddddddddddddddddddddddddddd ddddddddddddddddddddddddddddddddd
+                            ddddddddddddddddddddddddddddd ddddddddddddddddddddddddddddddddd
+                            ddddddddddddddddddddddddddddd ddddd
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
